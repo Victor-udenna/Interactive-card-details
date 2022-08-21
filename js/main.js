046509b2419cf07ab5c17bf7d8cb87e7b8642809
victorudenna@gmail.com <victorudenna@gmail.com>
2022-08-22 00:40:12 +0100

    Added thank you message functionality

--- js/main.js
+++ js/main.js
@@ -16,20 +16,48 @@ const cvc_input = document.querySelector('.cvc');
 let cvc_card = document.querySelector('.cvc-number');
 /////ERROR MESSAGES
 let ERROR_1 = document.querySelector('.error1');
-let ERROR_2 = document.querySelector('.error2');
+// let ERROR_2 = document.querySelector('.error2');
+let ERROR_2A = document.querySelector('.error2a');
 let ERROR_3 = document.querySelector('.error3');
-let ERROR_4 = document.querySelector('.error4');// confirm card btn.
+let ERROR_4 = document.querySelector('.error4');
+let ERROR_5 = document.querySelector('.error5');// confirm card btn.
 // Display
 let formDisplay = document.querySelector('.form_container');
 let thankyouDisplay = document.querySelector('.Thankyou_message');
+// All card content
+// let card__text = document.querySelectorAll('.card-text');
+// console.log(card__text);
+// let card__input = document.querySelectorAll('.card_input');
+// console.log(card__input);
 // CARD NAME CODE
 const inputName = () => {
-    card_holder_card.innerText = card_holder_input.value;
+        card_holder_card.innerText = card_holder_input.value;
+    if (card_holder_card.innerText = card_holder_input.value) {
+        card_holder_card.innerText = card_holder_input.value;
+        card_holder_input.style.borderColor = "#dedddf";
+        ERROR_1.style.visibility = "hidden";
+    } else {
+        card_holder_card.innerText = "Jane Appleseed";
+        card_holder_input.style.borderColor = "red";
+        ERROR_1.style.visibility = "visible";
+    }
 }
 //
 // CARD NUMBER CODE
 const inputNumber = () => {
     card_number_card.innerText = card_number_input.value;
+    if (card_number_card.innerText = card_number_input.value) {
+        card_number_card.innerText = card_number_input.value;
+        card_number_input.style.borderColor = "#dedddf";
+        ERROR_2A.style.visibility = "hidden";
+        // ERROR_2.style.visibility = "hidden";
+    } else  {
+        card_number_card.innerText = "0000 0000 0000 0000";
+        ERROR_2A.style.visibility = "visible";
+        // ERROR_2.style.visibility = "hidden";
+        card_number_input.style.borderColor = "red";
+    }
+
 }
 //
 // EXP DATE (month) MOnTH
@@ -38,11 +66,11 @@ const InputMonth = () => {
     if (expMonth_card.innerText = expMonth_input.value) {
         expMonth_card.innerText = expMonth_input.value;
         expMonth_input.style.borderColor = "#dedddf";
-        ERROR_2.style.visibility = "hidden";
+        ERROR_3.style.visibility = "hidden";
     } else {
         expMonth_input.style.borderColor = "red";
         expMonth_card.innerText = "00";
-        ERROR_2.style.visibility = "visible";
+        ERROR_3.style.visibility = "visible";
     }
 }
 // EXP DATE (month) MOnTH
@@ -51,11 +79,11 @@ const InputYear = () => {
     if (expYear_input.innerText = expYear_input.value) {
         expYear_card.innerText = expYear_input.value;
         expYear_input.style.borderColor = "#dedddf";
-        ERROR_3.style.visibility = "hidden";
+        ERROR_4.style.visibility = "hidden";
     } else {
         expYear_input.style.borderColor = "red";
         expYear_card.innerText = "00";
-        ERROR_3.style.visibility = "visible";
+        ERROR_4.style.visibility = "visible";
     }
 }
 // CVC NUMBER CODE
@@ -65,17 +93,26 @@ const Inputcvc = () => {
     if (cvc_card.innerText = cvc_input.value) {
         cvc_card.innerText = cvc_input.value;
         cvc_input.style.borderColor = "#dedddf";
-        ERROR_4.style.visibility = "hidden";
+        ERROR_5.style.visibility = "hidden";
     } else {
         cvc_input.style.borderColor = "red";
         cvc_card.innerText = "000";
-        ERROR_4.style.visibility = "visible";
+        ERROR_5.style.visibility = "visible";
     }
 }
 
 const chanegedisplay = () => {
-    formDisplay.style.display = "none";
-    thankyouDisplay.style.display = "flex";
+    if (cvc_card.innerText == cvc_input.value && expYear_card.innerText == expYear_input.value && expMonth_card.innerText == expMonth_input.value && card_number_card.innerText == card_number_input.value && card_holder_card.innerText == card_holder_input.value) {
+        console.log("Nice");
+        formDisplay.style.display = "none";
+        thankyouDisplay.style.display = "flex";
+    } 
+    
+    else {
+        console.log(' opps')
+        formDisplay.style.display = "block";
+        thankyouDisplay.style.display = "none";
+    }
 
 }
 
@@ -89,7 +126,7 @@ add_card.addEventListener('click', (event) => {
     InputMonth();
     InputYear();
     Inputcvc();
-    // chanegedisplay();
+    chanegedisplay();
 })
 
 function printfunction() {
